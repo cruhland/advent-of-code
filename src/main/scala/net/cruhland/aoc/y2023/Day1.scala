b0e71f8b35161d3b82a0bbbb867416b63e500bd5
Charles Ruhland <cruhland@alumni.cmu.edu>
2024-11-30 12:52:17 -0800

    Day 1: factored out common code for parts 1 and 2

--- src/main/scala/net/cruhland/aoc/y2023/Day1.scala
+++ src/main/scala/net/cruhland/aoc/y2023/Day1.scala
@@ -3,17 +3,21 @@ package net.cruhland.aoc.y2023
 object Day1 {
 
   def solution1(input: String): Int = {
+    common(input)(line => line.collect { case c if c.isDigit => c - '0' })
+  }
+
+  def common(input: String)(lineToDigits: String => Iterable[Int]): Int = {
     input
       .linesIterator
       .map { line =>
-        val digits = line.filter(_.isDigit)
+        val digits = lineToDigits(line)
         if (digits.isEmpty) 0
         else {
-          // These are safe because the string is non-empty
+          // These are safe because there is at least one digit
           val first = digits.head
           val last = digits.last
 
-          10 * (first - '0') + (last - '0')
+          10 * first + last
         }
       }
       .sum
