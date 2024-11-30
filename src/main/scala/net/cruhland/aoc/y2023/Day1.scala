1dbaee83be72e809fc862b673f713df6b659afc5
Charles Ruhland <cruhland@alumni.cmu.edu>
2024-11-30 15:47:56 -0800

    Day 1: organize code

--- src/main/scala/net/cruhland/aoc/y2023/Day1.scala
+++ src/main/scala/net/cruhland/aoc/y2023/Day1.scala
@@ -4,25 +4,22 @@ import scala.annotation.tailrec
 import scala.util.matching.Regex
 
 object Day1 {
-  private val NumbersPattern = "[1-9]"
-  private val WordsPattern = "one|two|three|four|five|six|seven|eight|nine"
 
-  val Numbers: Regex = NumbersPattern.r
-  val WordsOrNumbersForward: Regex = s"$NumbersPattern|$WordsPattern".r
-  val WordsOrNumbersReverse: Regex =
-    s"$NumbersPattern|${WordsPattern.reverse}".r
-
-  val DigitForWord: Map[String, Int] = Map(
-    "one" -> 1,
-    "two" -> 2,
-    "three" -> 3,
-    "four" -> 4,
-    "five" -> 5,
-    "six" -> 6,
-    "seven" -> 7,
-    "eight" -> 8,
-    "nine" -> 9,
-  )
+  def common(
+    input: String,
+    findFirst: String => Option[Int],
+    findLast: String => Option[Int],
+  ): Int = {
+    input
+      .linesIterator
+      .flatMap { line =>
+        for {
+          first <- findFirst(line)
+          last <- findLast(line)
+        } yield 10 * first + last
+      }
+      .sum
+  }
 
   def solution1(input: String): Int = {
     common(input, firstNumberToDigit, lastNumberToDigit)
@@ -58,20 +55,24 @@ object Day1 {
       .orElse(DigitForWord.get(numberOrWord))
   }
 
-  def common(
-    input: String,
-    findFirst: String => Option[Int],
-    findLast: String => Option[Int],
-  ): Int = {
-    input
-      .linesIterator
-      .flatMap { line =>
-        for {
-          first <- findFirst(line)
-          last <- findLast(line)
-        } yield 10 * first + last
-      }
-      .sum
-  }
+  val NumbersPattern: String = "[1-9]"
+  val WordsPattern: String = "one|two|three|four|five|six|seven|eight|nine"
+
+  val Numbers: Regex = NumbersPattern.r
+  val WordsOrNumbersForward: Regex = s"$NumbersPattern|$WordsPattern".r
+  val WordsOrNumbersReverse: Regex =
+    s"$NumbersPattern|${WordsPattern.reverse}".r
+
+  val DigitForWord: Map[String, Int] = Map(
+    "one" -> 1,
+    "two" -> 2,
+    "three" -> 3,
+    "four" -> 4,
+    "five" -> 5,
+    "six" -> 6,
+    "seven" -> 7,
+    "eight" -> 8,
+    "nine" -> 9,
+  )
 
 }
