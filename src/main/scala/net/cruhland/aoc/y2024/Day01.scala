b9ea5020e25ba107174e2b957c0e3c12dea626e5
Charles Ruhland <cruhland@alumni.cmu.edu>
2024-11-30 21:13:42 -0800

    Day 01: Stubs

---
+++ src/main/scala/net/cruhland/aoc/y2024/Day01.scala
@@ -0,0 +1,9 @@
+package net.cruhland.aoc.y2024
+
+object Day01 {
+
+  def solution1(input: String): Int = {
+    11
+  }
+
+}
---
+++ src/test/scala/net/cruhland/aoc/y2024/Day01Spec.scala
@@ -0,0 +1,26 @@
+package net.cruhland.aoc.y2024
+
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+
+class Day01Spec extends AnyFreeSpec with Matchers {
+
+  "Day01.solution1" - {
+
+    "is correct for the example input" in {
+      val input =
+        """3   4
+          |4   3
+          |2   5
+          |1   3
+          |3   9
+          |3   3
+        """.stripMargin
+
+      val answer = Day01.solution1(input)
+      answer mustBe 11
+    }
+
+  }
+
+}
