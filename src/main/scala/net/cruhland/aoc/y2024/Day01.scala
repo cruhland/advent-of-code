4f3c5e6c92a230f41773478f42fe05e1685d39e3
Charles Ruhland <cruhland@alumni.cmu.edu>
2024-11-30 21:43:09 -0800

    Day 01: part 1 solution

--- src/main/scala/net/cruhland/aoc/y2024/Day01.scala
+++ src/main/scala/net/cruhland/aoc/y2024/Day01.scala
@@ -5,12 +5,24 @@ import scala.util.matching.Regex
 object Day01 {
 
   def solution1(input: String): Int = {
-    if (input.isEmpty) 0
-    else if (input.contains('\n')) 11
-    else {
-      val Array(first, second) = Spaces.split(input).map(_.toInt)
-      math.abs(second - first)
-    }
+    // Parse both lists from the input
+    val (firstList, secondList) = input
+      .linesIterator
+      .flatMap { line =>
+        Spaces.split(line) match {
+          case Array(first, second, _*) => Some((first.toInt, second.toInt))
+          case _ => None
+        }
+      }
+      .toSeq
+      .unzip
+
+    // Sort both lists, pair them back together, and compute distance
+    firstList
+      .sorted
+      .zip(secondList.sorted)
+      .map { case (first, second) => math.abs(first - second) }
+      .sum
   }
 
   val Spaces: Regex = " +".r
