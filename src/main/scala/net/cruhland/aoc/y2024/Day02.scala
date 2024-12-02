c5a58bdb8957dce8c84eabc83f98f7a8dd5912d7
Charles Ruhland <cruhland@alumni.cmu.edu>
2024-12-01 22:06:19 -0800

    Day 02 part 1 complete solution, and part 2 stubs

--- src/main/scala/net/cruhland/aoc/y2024/Day02.scala
+++ src/main/scala/net/cruhland/aoc/y2024/Day02.scala
@@ -5,25 +5,31 @@ import scala.util.matching.Regex
 object Day02 {
 
   def solution1(input: String): Int = {
-    if (input.contains('\n')) 2
-    else {
-      if (validReport(input)) 1 else 0
-    }
+    input
+      .linesIterator
+      .count(validReport)
+  }
+
+  def solution2(input: String): Int = {
+    4
   }
 
   def validReport(report: String): Boolean = {
-    val numbers = report.split(' ').map(_.toInt)
-    val adjacentDiffs = numbers
-      .lazyZip(numbers.tail)
-      .map(_ - _)
+    val numbers = report.split(' ').flatMap(_.toIntOption)
+    if (numbers.isEmpty) false
+    else {
+      val adjacentDiffs = numbers
+        .lazyZip(numbers.tail)
+        .map(_ - _)
 
-    val allIncreasing = adjacentDiffs.forall(_ < 0)
-    val allDecreasing = adjacentDiffs.forall(_ > 0)
-    val diffsInRange = adjacentDiffs
-      .map(math.abs)
-      .forall(d => 1 <= d && d <= 3)
+      val allIncreasing = adjacentDiffs.forall(_ < 0)
+      val allDecreasing = adjacentDiffs.forall(_ > 0)
+      val diffsInRange = adjacentDiffs
+        .map(math.abs)
+        .forall(d => 1 <= d && d <= 3)
 
-    (allIncreasing || allDecreasing) && diffsInRange
+      (allIncreasing || allDecreasing) && diffsInRange
+    }
   }
 
 }
--- src/test/scala/net/cruhland/aoc/y2024/Day02Spec.scala
+++ src/test/scala/net/cruhland/aoc/y2024/Day02Spec.scala
@@ -36,6 +36,23 @@ class Day02Spec extends AnyFreeSpec with Matchers {
 
   }
 
+  "Day02.solution2" - {
+
+    "example input" in {
+      val input =
+        """7 6 4 2 1
+          |1 2 7 8 9
+          |9 7 6 2 1
+          |1 3 2 4 5
+          |8 6 4 4 1
+          |1 3 6 7 9
+        """.stripMargin
+
+      val answer = solution2(input)
+      answer mustBe 4
+    }
+  }
+
   "Day02.validReport" - {
 
     "example 1" in {
