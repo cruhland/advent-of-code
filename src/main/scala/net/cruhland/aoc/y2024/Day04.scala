2b6af8cf226c2db8af782666b6e0221be46da9b4
Charles Ruhland <cruhland@alumni.cmu.edu>
2024-12-07 15:31:51 -0800

    Day 04 part 1: add comments

--- src/main/scala/net/cruhland/aoc/y2024/Day04.scala
+++ src/main/scala/net/cruhland/aoc/y2024/Day04.scala
@@ -3,14 +3,17 @@ package net.cruhland.aoc.y2024
 object Day04 {
 
   def solution1(input: String): Int = {
+    // Make a 2D grid of chars
     val charMatrix = input
       .split('\n')
       .toVector
       .map(Vector(_: _*))
 
+    // Projections of the grid into rows by direction
     val horizontal = charMatrix.view
     val vertical = charMatrix.transpose(identity).view
 
+    // Count words forward and backward in each projection
     List(horizontal, vertical)
       .flatMap(rows => List(rows, rows.map(_.reverse)))
       .map(countWords)
