67c854ae7516965e9a69c19be6322a4240ebf45d
Charles Ruhland <cruhland@alumni.cmu.edu>
2024-12-11 07:06:04 -0800

    Day05 part 1: all updates are valid with no rules

--- src/main/scala/net/cruhland/aoc/y2024/Day05.scala
+++ src/main/scala/net/cruhland/aoc/y2024/Day05.scala
@@ -2,6 +2,17 @@ package net.cruhland.aoc.y2024
 
 object Day05 {
 
-  def solution1(input: String): Int = ???
+  def solution1(input: String): Int = {
+    // Parse input into data structures (ordering rules, possible updates)
+    // Keep only the updates that are valid according to the rules
+    // Add up the middle page numbers of the valid updates
+    ???
+  }
+
+  def isUpdateValid[A](update: Iterable[A], rules: OrderingRules): Boolean = {
+    true
+  }
+
+  class OrderingRules
 
 }
--- src/test/scala/net/cruhland/aoc/y2024/Day05Spec.scala
+++ src/test/scala/net/cruhland/aoc/y2024/Day05Spec.scala
@@ -6,6 +6,17 @@ import org.scalatest.matchers.must.Matchers
 class Day05Spec extends AnyFreeSpec with Matchers {
   import Day05._
 
+  "Day05.isUpdateValid" - {
+
+    "no rules, update always valid" in {
+      val rules = new OrderingRules
+      val update = Seq('a', 'b', 'c')
+      val result = isUpdateValid(update, rules)
+      result mustBe true
+    }
+
+  }
+
   "Day05.solution1" - {
 
   }
