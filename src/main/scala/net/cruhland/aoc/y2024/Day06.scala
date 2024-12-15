60297a1e04f0f89ec83c3747dda1614891a0abc0
Charles Ruhland <cruhland@alumni.cmu.edu>
2024-12-14 20:17:52 -0800

    Day 06 part 1: parse blank map of any size

--- src/main/scala/net/cruhland/aoc/y2024/Day06.scala
+++ src/main/scala/net/cruhland/aoc/y2024/Day06.scala
@@ -2,4 +2,26 @@ package net.cruhland.aoc.y2024
 
 object Day06 {
 
+  def solution1(input: String): Int = {
+    // Parse the input into a model of the state
+    // While the guard has not left the board, step the state forward
+    // Count positions visited
+
+    // The state model will need to have the following information:
+    // - Static: Dimensions of the map
+    // - Static: Positions of all obstacles
+    // - Dynamic: Optional position of the guard (None if not on map)
+    // - Dynamic: Direction of the guard
+    // - Dynamic: Which positions have been visited (to provide count)
+    ???
+  }
+
+  def parse(input: String): StartingState = {
+    val width = input.indexOf('\n')
+    val height = input.count(_ == '\n') + 1
+    StartingState(width = width, height = height)
+  }
+
+  case class StartingState(width: Int, height: Int)
+
 }
--- src/test/scala/net/cruhland/aoc/y2024/Day06Spec.scala
+++ src/test/scala/net/cruhland/aoc/y2024/Day06Spec.scala
@@ -5,4 +5,22 @@ import org.scalatest.matchers.must.Matchers
 
 class Day06Spec extends AnyFreeSpec with Matchers {
 
+  "parse" - {
+
+    "map dimensions" - {
+      def testMapDimensions(width: Int, height: Int): Boolean = {
+        val row = "." * width
+        val input = Iterator
+          .fill(height)(row)
+          .mkString("\n")
+
+        val startState = Day06.parse(input)
+        startState.width == width && startState.height == height
+      }
+
+      "small map" in assert(testMapDimensions(width = 4, height = 3))
+      "medium map" in assert(testMapDimensions(width = 17, height = 71))
+    }
+  }
+
 }
